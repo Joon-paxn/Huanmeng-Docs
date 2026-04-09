85ecf78a6ddee531dd96734f1fb340f87de4b643
BingKKni <xbudingqwq@gmail.com>
2026-04-09 16:06:46 +0800

    index.md 表格暂时移除；新增打卡页面做侧边标签栏测试

--- src/.vitepress/config.mjs
+++ src/.vitepress/config.mjs
@@ -7,6 +7,14 @@ export default defineConfig({
   head: [
     ['link', { rel: 'icon', type: 'image/png', href: '/img/hm_icon.png' }]
   ],
+  markdown: {
+    container: {
+      tipLabel: '提示',
+      warningLabel: '注意',
+      dangerLabel: '注意',
+      infoLabel: '信息'
+    }
+  },
   vite: {
     build: {
       // Preserve existing deployment files like .user.ini in the output directory.
