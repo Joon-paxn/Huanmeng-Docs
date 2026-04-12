49d949ce72917254550e994284941bf16266155a
xiaobudingqwq <xbudingqwq@gmail.com>
2026-04-12 20:12:52 +0800

    优化文档UI

--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -1,5 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fontsource-variable/inter'
+import '@fontsource/noto-sans-sc/400.css'
+import '@fontsource/noto-sans-sc/500.css'
+import '@fontsource/noto-sans-sc/700.css'
 import Layout from './Layout.vue'
 import './style.css'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
